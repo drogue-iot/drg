00bd7b03f55d31c90189f3742d223548f89c8414
Jens Reimann <jreimann@redhat.com>
2021-04-30 11:51:37 +0200

    Add a windows build

--- .github/scripts/package_release.sh
+++ .github/scripts/package_release.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-OS=`uname -s`
-ARCH=`uname -m`
+set -xe
 
-mv target/release/drg drg
-tar -czf drg-"$OS"_"$ARCH".tar.gz README.md LICENSE drg
+SUFFIX="$1"
+EXE="$2"
+TYPE="$3"
+VERSION="$4"
+
+# copy, don't move, as we might need it later
+cp "target/release/drg${EXE}" "drg${EXE}"
+case "$TYPE" in
+  "tar.gz")
+    tar -czf drg-"$VERSION"-"$SUFFIX".tar.gz README.md LICENSE "drg${EXE}"
+    ;;
+  "zip")
+    7z a -tzip drg-"$VERSION"-"$SUFFIX".zip README.md LICENSE "drg${EXE}"
+    ;;
+esac
