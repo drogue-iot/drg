6244083e562ef9b179d94e92887d8f3310dfb764
jbtrystram <jbtrystram@redhat.com>
2021-12-14 15:18:49 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,9 @@ fn main() -> Result<()> {
                     token_exp_date: chrono::MAX_DATETIME,
                 })
             } else {
-                Err(anyhow!("Invalid access token. Format should be username:token"))
+                Err(anyhow!(
+                    "Invalid access token. Format should be username:token"
+                ))
             }
         } else {
             let refresh_token_val = submatches.unwrap().value_of(Other_commands::token);
