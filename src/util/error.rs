06215f3678d40bc2ab7ffb04116b7bf1e0a36353
Jean-Baptiste Trystram <jbtrystram@redhat.com>
2022-05-25 15:30:21 +0200

    update to drogue-client error changes

--- src/util/error.rs
+++ src/util/error.rs
@@ -22,7 +22,13 @@ impl From<ClientError> for DrogueError {
         match error {
             ClientError::Client(e) => DrogueError::UnexpectedClient(anyhow!(e)),
             ClientError::Request(msg) => DrogueError::UnexpectedClient(anyhow!("{}", msg)),
-            ClientError::Service(e) => DrogueError::Service(e.message, e.status.as_u16()),
+            ClientError::Service {
+                code: c,
+                error: msg,
+            } => DrogueError::Service(msg.message, c.as_u16()),
+            ClientError::Response(c) => {
+                DrogueError::Service("Unknown error".to_string(), c.as_u16())
+            }
             ClientError::Token(e) => DrogueError::UnexpectedClient(anyhow!(e)),
             ClientError::Url(e) => DrogueError::ConfigIssue(format!("Invalid url: {}", e)),
             ClientError::Syntax(e) => {
