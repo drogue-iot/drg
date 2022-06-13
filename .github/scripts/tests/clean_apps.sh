05bf95d88e751afb78c4f4b158524b875bab9566
jbtrystram <jbtrystram@redhat.com>
2022-06-13 12:09:46 +0200

    [fix] fix clean apps script for unit tests

--- .github/scripts/tests/clean_apps.sh
+++ .github/scripts/tests/clean_apps.sh
@@ -2,7 +2,7 @@
 
 drg login "$DROGUE_SANDBOX_URL" --access-token "$DROGUE_SANDBOX_USERNAME":"$DROGUE_SANDBOX_ACCESS_KEY"
 
-for a in $(drg get apps | awk 'NR>2 {print $1}')
+for a in $(drg get apps | awk 'NR>1 {print $1}')
 do
    drg delete app "$a"
 done
